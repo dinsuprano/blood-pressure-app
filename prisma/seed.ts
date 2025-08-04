91a0a0738b0af13691ada124e6d30bf91909d3d0
dinsuprano <dinsuprano@gmail.com>
2025-08-05 00:42:05 +0800

    rebuild0-1

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,30 +1,30 @@
-// // prisma/seed.ts
-// import { PrismaClient } from '@prisma/client'
-// import bcrypt from 'bcrypt'
+// prisma/seed.ts
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// async function main() {
-//   const password = await bcrypt.hash("Intan226@", 10)
+async function main() {
+  const password = await bcrypt.hash("Intan226@", 10)
 
-//   await prisma.user.upsert({
-//     where: { email: "dinsuprano@gmail.com" },
-//     update: {},
-//     create: {
-//       email: "dinsuprano@gmail.com",
-//       password,
-//     },
-//   })
-// }
+  await prisma.user.upsert({
+    where: { email: "dinsuprano@gmail.com" },
+    update: {},
+    create: {
+      email: "dinsuprano@gmail.com",
+      password,
+    },
+  })
+}
 
-// main()
-//   .then(() => {
-//     console.log("✅ Seeded admin user")
-//   })
-//   .catch((e) => {
-//     console.error("❌ Error seeding:", e)
-//     process.exit(1)
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect()
-//   })
+main()
+  .then(() => {
+    console.log("✅ Seeded admin user")
+  })
+  .catch((e) => {
+    console.error("❌ Error seeding:", e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
